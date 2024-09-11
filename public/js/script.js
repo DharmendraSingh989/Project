9e1d336294b4f068088bcdb1ecf0a89afb023736
Avaraboy <avaraboypc@gmail.com>
2024-09-12 01:56:43 +0530

    this is project code

--- public/js/script.js
+++ public/js/script.js
@@ -17,6 +17,26 @@ if(navigator.geolocation){
     );
 }
 
-const map = L.map("map").setView([0,0],10);
+const map = L.map("map").setView([0,0],20);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{attribution:"OpenStreetMap"}).addTo(map)
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{attribution:"By SheldHer Team"}).addTo(map)
+
+const markers = {};
+
+socket.on("receive-location",(data)=>{
+    const{id,latitude,longitude}=data;
+    map.setView([latitude,longitude]);
+    if (markers[id]) {
+        markers[id].setLatLng([latitude,longitude]);
+    }
+    else{
+        markers[id]=L.marker([latitude,longitude]).addTo(map);
+    }
+});
+
+socket.on("user-disconnected",(id)=>{
+    if(markers[id]){
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
+})
